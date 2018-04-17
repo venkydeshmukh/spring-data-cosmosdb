31a7b59fd0c4f1d5ddf14a0c8fadaff6d7e647f6
Pan Li <panli@microsoft.com>
2018-04-17 13:19:05 +0800

    Add IT test for travis CI.
    * Appveyor only focus on build.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/libs/setup.sh
+++ src/libs/setup.sh
@@ -3,6 +3,10 @@
 resourcegroup=$1
 dbname=$2
 
+echo ===================== Start Setup ==============
+
+cat ./src/test/resources/application.properties
+
 if [ -z "$CLIENT_ID" ]; then
     exit 0
 fi
@@ -12,6 +16,8 @@ fi
 if [ -z "$TENANT_ID" ]; then
     exit 0
 fi
+
+cat ./src/test/resources/application.properties
 
 az login --service-principal -u $CLIENT_ID -p $CLIENT_KEY --tenant $TENANT_ID >> tmp.txt
 documentDbUri=$(az cosmosdb create --name $dbname --resource-group $resourcegroup --kind GlobalDocumentDB --query documentEndpoint)
